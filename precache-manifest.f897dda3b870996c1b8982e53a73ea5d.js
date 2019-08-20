ca69857d4a8f51af3dbaad82ff0ac07964b6d05a
CorbicBojana <CorbicBojana@gmail.com>
2019-08-21 01:05:21 +0200

    Updates

---
+++ precache-manifest.f897dda3b870996c1b8982e53a73ea5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5f3779b7a933ee5bb81b3c41c3db6f",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "6620c71239794b9a64b2",
+    "url": "/to-do-app/static/css/main.1479644f.chunk.css"
+  },
+  {
+    "revision": "39e8038a90df0edaa718",
+    "url": "/to-do-app/static/js/2.25e96185.chunk.js"
+  },
+  {
+    "revision": "6620c71239794b9a64b2",
+    "url": "/to-do-app/static/js/main.87e1b03d.chunk.js"
+  },
+  {
+    "revision": "ed691c71e70117f66e2b",
+    "url": "/to-do-app/static/js/runtime~main.f282790d.js"
+  }
+]);
